dba0817b71b585a614170fbf62958e06c923687f
sihogu <73099483+sihogu@users.noreply.github.com>
2024-11-08 17:53:21 +0900

    Add files via upload

---
+++ processing.py
@@ -0,0 +1,41 @@
+import yaml
+import os
+import json
+
+#next.yaml의 path 수정
+def update_yaml_path(current_dir, dir_name):
+    yaml_file = os.path.join(current_dir,dir_name,'nextchip.yaml')
+
+    with open (yaml_file, 'r',encoding='utf-8') as file:
+        yaml_data = yaml.safe_load(file)
+    yaml_data['path'] = dir_name
+
+    with open(yaml_file, 'w') as file:
+        yaml.dump(yaml_data, file, default_flow_style=False)
+    
+    print(f"Updated path to {dir_name} in {yaml_file}")
+
+#욜로 데이터셋 지정경로를 강제로 현재 경로로
+def update_datasets_dir():   
+    settings_path = os.path.join(os.getenv('APPDATA'), 'Ultralytics', 'settings.json')
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    
+    if os.path.exists(settings_path):
+        with open(settings_path, 'r+') as f:
+            settings = json.load(f)
+            settings['datasets_dir'] = current_dir
+            f.seek(0)
+            json.dump(settings, f, indent=4)
+            f.truncate()
+        print(f"Updated datasets_dir to {current_dir}")
+    else:
+        print("Ultralytics settings.json file not found.")
+
+def main(dir_name='datasets/nextchip'):
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    update_datasets_dir()
+    update_yaml_path(current_dir, dir_name)
+    
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -5,39 +5,9 @@ import argparse
 from ultralytics import YOLO
 import torch
 import sys
-import json
-import yaml
 
 torch.cuda.empty_cache()
 
-#next.yaml의 path 수정
-def update_yaml_path(current_dir,dir_name):
-    yaml_file = os.path.join(current_dir,dir_name,'nextchip.yaml')
-
-    with open (yaml_file, 'r',encoding='utf-8') as file:
-        yaml_data = yaml.safe_load(file)
-    yaml_data['path'] = 'datasets/nextchip'
-
-    with open(yaml_file, 'w') as file:
-        yaml.dump(yaml_data, file, default_flow_style=False)
-    
-    print(f"Updated path to {current_dir, dir_name} in {yaml_file}")
-
-#욜로 데이터셋 지정경로를 강제로 현재 경로로
-def update_datasets_dir():   
-    settings_path = os.path.join(os.getenv('APPDATA'), 'Ultralytics', 'settings.json')
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    
-    if os.path.exists(settings_path):
-        with open(settings_path, 'r+') as f:
-            settings = json.load(f)
-            settings['datasets_dir'] = current_dir
-            f.seek(0)
-            json.dump(settings, f, indent=4)
-            f.truncate()
-        print(f"Updated datasets_dir to {current_dir}")
-    else:
-        print("Ultralytics settings.json file not found.")
 
 def parse_device(value):
     if value.lower() == 'cpu':
@@ -75,19 +45,15 @@ def parse_args():
 
 def main():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    update_datasets_dir()
-
     args = parse_args()
 
     if 'datasets' not in args.dir_name:
         dir_name = os.path.join('datasets',args.dir_name)
     else:
         dir_name = args.dir_name
-        
-    device = args.device
-    workers = args.workers
     
-    update_yaml_path(current_dir,dir_name)
+    import processing
+    processing.main(dir_name)
 
     if args.use_merge:
         print("Merging test set into train set...")
@@ -96,9 +62,9 @@ def main():
         merge.main()
     
     model = YOLO(os.path.join(current_dir,'config','deepdrive.yaml'))
-
-    
     data_path = os.path.join(current_dir, dir_name, 'nextchip.yaml')
+    device = args.device
+    workers = args.workers
 
     results_train = model.train(
         data=data_path,
